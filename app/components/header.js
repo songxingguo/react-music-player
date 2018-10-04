cf8a78ed6d7a9f3708b50e2136d73e3eeb77ca53
songxingguo <1328989942@qqc.com>
2018-10-05 01:37:35 +0800

    打包路径修改

--- app/components/header.js
+++ app/components/header.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import './header.less'
+import './header.less';
+
+import logo from './logo.png';
 
 let Header = React.createClass({
     render() {
         return (
-        	<div className="components-header row">
-        		<img src="/static/images/logo.png" width="40" alt="23" className="-col-auto" />
-        		<h1 className="caption">React Music Player</h1>	
-         	</div>
+            <div className="components-header row">
+                <img src={logo} width="40" alt="23" className="-col-auto" />
+                <h1 className="caption">React Music Player</h1>
+            </div>
         );
     }
 });
