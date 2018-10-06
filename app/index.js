c97e0c4e600b46fa3edec3db8b9fc3d971bd9c72
songxingguo <1328989942@qqc.com>
2018-10-07 01:13:25 +0800

    解决样式路径问题

--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { render } from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 import Root from './root'
+import  '../static/css/common.css'
+import  '../static/css/reset.css'
 
 render(
 	<AppContainer>
